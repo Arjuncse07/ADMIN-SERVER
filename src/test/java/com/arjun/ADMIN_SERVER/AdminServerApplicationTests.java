9ec24b57ee9d94e6095b16122bd5e69e8669b91e
Arjun Singh <88672863+Arjuncse07@users.noreply.github.com>
2023-12-26 23:22:32 +0530

    ADMIN-SERVER Changes

---
+++ src/test/java/com/arjun/ADMIN_SERVER/AdminServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.arjun.ADMIN_SERVER;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
